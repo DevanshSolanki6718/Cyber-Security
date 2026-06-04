49c3327299162b0f330dc16ff9b1ad2405ca1876
DevanshSolanki6718 <dsolanki0718@gmail.com>
2026-06-04 00:53:51 -0500

    Modified the whole code

--- Python_Scripts/mac_changer.py
+++ Python_Scripts/mac_changer.py
@@ -1,42 +1,80 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import subprocess, optparse, re
-
-def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    (options, arguments) = parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Please specify an interface, --help for more info.")
-    elif not options.new_mac:
-        parser.error("[-] Please specify a new mac, --help for more info.")
-    return options
+import subprocess
+import re
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_addr_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    try:
+        ifconfig_result = subprocess.check_output(
+            ["ifconfig", interface]
+        ).decode()
 
-    if mac_addr_search_result:
-        return mac_addr_search_result.group(0)
-    else:
-        print("[-] Could not read the MAC address")
+        mac_addr_search_result = re.search(
+            r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",
+            ifconfig_result
+        )
 
-options = get_arguments()
+        if mac_addr_search_result:
+            return mac_addr_search_result.group(0)
+        else:
+            print("[-] Could not read MAC address")
+            return None
 
-current_mac = get_current_mac(options.interface)
-print("Current MAC = " + str(current_mac))
+    except subprocess.CalledProcessError:
+        print("[-] Invalid interface")
+        return None
 
-change_mac(options.interface, options.new_mac)
+    except Exception as e:
+        print("[-] Error:", e)
+        return None
 
-current_mac = get_current_mac(options.interface)
-if current_mac == options.new_mac:
-    print("[+] MAC address was successfully changed to " + current_mac)
+print("\n========== MAC CHANGER ==========")
+print("1. Check Current MAC")
+print("2. Change MAC Address")
+print("3. Exit")
+
+choice = input("\nEnter your choice: ")
+
+if choice == "1":
+
+    interface = input("Enter interface name: ")
+
+    current_mac = get_current_mac(interface)
+
+    if current_mac:
+        print("[+] Current MAC Address =", current_mac)
+
+elif choice == "2":
+    interface = input("Enter interface: ")
+    new_mac = input("Enter new MAC Address: ")
+
+    current_mac = get_current_mac(interface)
+
+    if current_mac:
+        print("[+] Current MAC =", current_mac)
+
+        change_mac(interface, new_mac)
+
+        current_mac = get_current_mac(interface)
+
+        if current_mac == new_mac:
+            print(
+                "[+] MAC address was successfully changed to "
+                + current_mac
+            )
+        else:
+            print(
+                "[-] Sorry! MAC address did not get changed."
+            )
+
+elif choice == "3":
+    print("[+] Exiting program...")
 else:
-    print("[-] Sorry! MAC address did not get changed.")
+    print("[-] Invalid option. Please choose 1, 2, or 3.")
